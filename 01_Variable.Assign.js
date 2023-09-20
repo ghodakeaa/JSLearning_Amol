b924c66b6e943b431c7384663d6c38e267fc9ebd
ghodakeaa <ghodakeaa@gmail.com>
2023-09-20 15:06:44 +0530

    Variable Declaration
    Variable Declaration

---
+++ 01_Variable.Assign.js
@@ -0,0 +1,26 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName="Amol Ghodake";
+console.log(myFullName);
+
+myFullName="Amol Arjun Ghodake";
+console.log(myFullName);
+
+console.log("\n"); 
+
+var myLove="I love only JS";
+console.log(myLove);
+
+console.log("\n"); 
+
+var firstName="Amol";
+var lastName="Ghodake";
+var age="28 Years";
+var address="Bangalore";
+var pinCode= 560066;
+var gender= "Male";
+var isMarried= "Yes";
+console.log("First Name:"+ firstName+",", "Last Name:"+ lastName+",","Age:" +age+",", "Address:"+ address+",", "PinCode:" +pinCode+",", "Gender:"+ gender+",", "Marital Status:"+ isMarried); 
+
+console.log("\n"); 
---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+var firstName; //variable declaration
+firstName="Amol"; //variable insalivation
+
+console.log(firstName);
+firstName="Mr. Amol";
+console.log(firstName);
+
+var lastName="Ghodake"; //variable declaration & variable insalivation
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.2 feet";// update
+console.log(height);
