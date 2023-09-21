37fc85396a1975a8d18d1ce16480dc856272690f
ghodakeaa <ghodakeaa@gmail.com>
2023-09-21 15:12:18 +0530

    Data type with typeof
    Data type with typeof

---
+++ 02_typeOf.js
@@ -0,0 +1,24 @@
+console.log("-----------------Assignment 2-----------------\n");
+
+var bankName;
+console.log("Bank Name:",bankName ,",", "Type:", typeof bankName);
+
+bankName= "SBI Bank";
+console.log("Bank Name:",bankName ,",", "Type:", typeof bankName);
+console.log("****************************************************************\n");
+
+var marks="90%"
+console.log("Marks:", marks, ",", "Type:", typeof marks);
+console.log("****************************************************************\n");
+
+var isWorking;
+console.log("Is Working:", isWorking, ",", "Type:", typeof isWorking);
+isWorking=true;
+console.log("Is Working:", isWorking, ",", "Type:", typeof isWorking);
+console.log("****************************************************************\n");
+
+var totalCount="Hundred and Seven";
+console.log("Total Count:", totalCount, ",", "Type:", typeof totalCount);
+totalCount=107;
+console.log("Total Count:", totalCount, ",", "Type:", typeof totalCount);
+console.log("****************************************************************");
---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,30 @@
+console.log("Following are the examples of number, string and boolean variables \n");
+console.log("Number");
+var mNo=9322202110;
+var accountNumber=1234567890;
+var salary=123456;
+
+console.log("\n","Mobile Number:", mNo,",","Type:",typeof mNo,"\n","Account Number:",accountNumber,",","Type:" ,typeof accountNumber, "\n", "Salary:",salary,",","Type:" ,typeof salary);
+console.log("****************************************************************\n");
+console.log("String");
+var animal="Tiger";
+var bird="Peacock";
+var flower="Lotus"
+
+console.log("\n","National animal of India:",animal,",", "Type:", typeof animal,"\n", "National bird of India:", bird,",", "Type:", typeof bird, "\n", "National flower of India:", flower, ",", "Type:", typeof flower);
+
+console.log("****************************************************************\n");
+console.log("Boolean");
+
+var isIndiaOnMoon=true;
+var cherriesColorRed=true;
+var bonesInHuman150=false;
+
+console.log("\n","India is on Moon:", isIndiaOnMoon, ",", "Type:",typeof isIndiaOnMoon, "\n", "The color of cherries is red:",cherriesColorRed, ",", "Type:", typeof cherriesColorRed, "\n", "The human body consists of 150 bones:", bonesInHuman150, ",", "Type", typeof bonesInHuman150);
+
+console.log("****************************************************************\n");
+console.log("---------------Difference between null & undefined---------------");
+
+console.log("Null-\n Null means that there is no value, It does not contains 0 also, 0 means the value itself is 0.\n ");
+console.log("Undefined-\n Undefined means a variable has been declared but has yet not been assigned a value.")
+console.log("****************************************************************");
